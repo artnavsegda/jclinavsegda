ae78e3c59a981ec5b3b37bcbdf7c0074f486574d
artnavsegda@gmail.com <artnavsegda@gmail.com>
2020-05-18 14:21:47 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 var IRZ = require ('irz_module');
 print("hello");
+IRZ.system("echo system hello");
--- src/irz-module.c
+++ src/irz-module.c
@@ -46,8 +46,6 @@ static jerry_value_t module_cat_handler(const jerry_value_t function_object, con
 
     return returnvalue;
   }
-  else
-    printf ("Cat handler was called\n");
 
   return jerry_create_undefined();
 }
@@ -60,21 +58,13 @@ static jerry_value_t module_system_handler(const jerry_value_t function_object, 
     jerry_char_t buffer[256];
     jerry_size_t copied_bytes = jerry_string_to_utf8_char_buffer (string_value, buffer, sizeof (buffer) - 1);
     buffer[copied_bytes] = '\0';
-
     jerry_release_value (string_value);
-
-    printf ("System was called with args %s\n", (const char *)buffer);
-
     system(buffer);
-
-    //printf("contents: %s",filecontent);
-
     return jerry_create_undefined();
   }
   else
     printf ("System handler was called\n");
 
-  /* Return an "undefined" value to the JavaScript engine */
   return jerry_create_undefined();
 }
 
@@ -86,31 +76,17 @@ static jerry_value_t module_pipe_handler(const jerry_value_t function_object, co
     jerry_char_t buffer[256];
     jerry_size_t copied_bytes = jerry_string_to_utf8_char_buffer (string_value, buffer, sizeof (buffer) - 1);
     buffer[copied_bytes] = '\0';
-
     jerry_release_value (string_value);
-
-    printf ("Pipe was called with args %s\n", (const char *)buffer);
-
     FILE * filetoread = popen(buffer, "r");
-
     char filecontent[1000] = "empty";
-
     if (filetoread)
     {
       fread(filecontent,1000,1,filetoread);
       fclose(filetoread);
     }
-
-    //printf("contents: %s",filecontent);
-
     jerry_value_t returnvalue = jerry_create_string (filecontent);
-
     return returnvalue;
   }
-  else
-    printf ("Joke handler was called\n");
-
-  /* Return an "undefined" value to the JavaScript engine */
   return jerry_create_undefined();
 }
 
@@ -119,7 +95,7 @@ static void register_module_js_function (jerry_value_t module, const char *name_
   jerry_value_t func_obj = jerry_create_external_function (handler_p);
   jerry_release_value (jerryx_set_property_str (module, name_p, func_obj));
   jerry_release_value (func_obj);
-} /* register_module_js_function */
+}
 
 static jerry_value_t irz_module_on_resolve (void)
 {
@@ -130,6 +106,6 @@ static jerry_value_t irz_module_on_resolve (void)
   register_module_js_function(object, "pipe", module_pipe_handler);
 
   return object;
-} /* my_custom_module_on_resolve */
+}
 
 JERRYX_NATIVE_MODULE (MODULE_NAME, irz_module_on_resolve)
