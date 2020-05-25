3b9d6c97f8d975b4883071c67aa4d57fde31dd5e
artnavsegda@gmail.com <artnavsegda@gmail.com>
2020-05-25 14:25:33 +0300

    okay

---
+++ src/cli.js
@@ -0,0 +1,133 @@
+
+class Proto {
+  constructor(protoname, filelist, filepath) {
+    this.facelist = [];
+    this.name = protoname;
+    if (filelist)
+      this.load(filepath, filelist)
+  }
+  add(newelement) {
+    this.facelist.push(newelement);
+  }
+  basename(filename)
+  {
+    var path = filename.split('/');
+    var basename = path[path.length-1].split('.')
+    return basename[0];
+  }
+  load(filepath, filelist) {
+    if (filelist) {
+      this.facelist = [];
+      filelist.forEach((filename) => {
+        var path = filename.split('/');
+        var attachproto = this;
+        path.forEach((item, i) => {
+          if (i > 0) {
+            if (i == path.length-1)
+              attachproto.load(filepath + filename);
+            else {
+              var attachmaybe = attachproto.traverse(item);
+              if (!attachmaybe)
+              {
+                attachmaybe = new Proto(item);
+                attachproto.facelist.push(attachmaybe);
+              }
+              attachproto = attachmaybe;
+            }
+          }
+        });
+      });
+    } else {
+      var data = JSON.parse(IRZ.cat(filepath))
+      if (data.properties)
+        this.facelist.push(new Option(data, this.basename(filepath)));
+      else
+        this.facelist.push(new Face(data, this.basename(filepath)));
+    }
+  }
+  list() {
+    var protolist = [];
+    this.facelist.forEach((element) => protolist.push(element.name));
+    return protolist;
+  }
+  traverse(command) {
+    return this.facelist.find((element) => element.name == command);
+  }
+}
+
+class Face {
+  constructor(schema, name, data) {
+    this.schema = schema;
+    this.name = name;
+    if (data) {
+      this.data = data;
+    } else if (this.schema.acquire) {
+      var now_script_path = config.script_path + "/" + this.schema.acquire.exec + " " + this.schema.acquire.args.join(" ");
+      //print("piping " + now_script_path);
+      var now_data = IRZ.pipe(now_script_path);
+      if (now_data)
+        this.data = JSON.parse(now_data);
+    }
+  }
+  list() {
+    if (this.schema.namesake) {
+      var facelist = [];
+      Object.getOwnPropertyNames(this.data).forEach((element) => facelist.push(this.data[element][this.schema.namesake]));
+      return facelist;
+    }
+    else
+      return Object.getOwnPropertyNames(this.data);
+  }
+  resolve(dataname) {
+    if (this.data[dataname]) {
+      //print("data: " + JSON.stringify(this.data[command]));
+      var schema_section;
+      Object.getOwnPropertyNames(this.schema.patternProperties).forEach((pattern) => {
+        let re = new RegExp(pattern);
+        if (re.test(dataname))
+          schema_section = this.schema.patternProperties[pattern];
+      });
+      //print("schema: " + JSON.stringify(schema_section));
+      return schema_section;
+    }
+    return undefined
+  }
+  traverse(command) {
+    if (this.schema.namesake) {
+      var dataname = Object.getOwnPropertyNames(this.data).find((element) => command == this.data[element][this.schema.namesake])
+      if (this.resolve(dataname))
+        return new Option(this.resolve(dataname), command, this.data[dataname])
+    }
+    else
+      if (this.resolve(command))
+        return new Option(this.resolve(command), command, this.data[command])
+    return undefined;
+  }
+}
+
+class Option {
+  constructor(schema, name, data) {
+    this.schema = schema;
+    this.name = name;
+    if (data) {
+      this.data = data;
+    } else if (this.schema.acquire) {
+      var now_script_path = config.script_path + "/" + this.schema.acquire.exec + " " + this.schema.acquire.args.join(" ");
+      //print("piping " + now_script_path);
+      var now_data = IRZ.pipe(now_script_path);
+      if (now_data)
+        this.data = JSON.parse(now_data);
+    }
+  }
+  list() {
+    return Object.getOwnPropertyNames(this.schema.properties);
+  }
+  traverse(command) {
+    //print("schema: " + JSON.stringify(this.schema.properties[command]));
+    //print("data: " + this.data[command]);
+    print(this.data[command]);
+    return undefined;
+  }
+}
+
+export { Proto, Face, Option };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as jsonpointer from "jsonpointer.js";
+import * as CLI from "cli.js";
 var IRZ = require ('irz_module');
 var config = JSON.parse(IRZ.cat("./config.json"));
 var prompt = IRZ.getenv("USER") + "@" + IRZ.getenv("HOSTNAME") +"/>";
@@ -32,137 +33,6 @@ var state = {
     this.location = this.path[this.path.length-1];
   }
 };
-
-class Proto {
-  constructor(protoname, filelist, filepath) {
-    this.facelist = [];
-    this.name = protoname;
-    if (filelist)
-      this.load(filepath, filelist)
-  }
-  add(newelement) {
-    this.facelist.push(newelement);
-  }
-  basename(filename)
-  {
-    var path = filename.split('/');
-    var basename = path[path.length-1].split('.')
-    return basename[0];
-  }
-  load(filepath, filelist) {
-    if (filelist) {
-      this.facelist = [];
-      filelist.forEach((filename) => {
-        var path = filename.split('/');
-        var attachproto = this;
-        path.forEach((item, i) => {
-          if (i > 0) {
-            if (i == path.length-1)
-              attachproto.load(filepath + filename);
-            else {
-              var attachmaybe = attachproto.traverse(item);
-              if (!attachmaybe)
-              {
-                attachmaybe = new Proto(item);
-                attachproto.facelist.push(attachmaybe);
-              }
-              attachproto = attachmaybe;
-            }
-          }
-        });
-      });
-    } else {
-      var data = JSON.parse(IRZ.cat(filepath))
-      if (data.properties)
-        this.facelist.push(new Option(data, this.basename(filepath)));
-      else
-        this.facelist.push(new Face(data, this.basename(filepath)));
-    }
-  }
-  list() {
-    var protolist = [];
-    this.facelist.forEach((element) => protolist.push(element.name));
-    return protolist;
-  }
-  traverse(command) {
-    return this.facelist.find((element) => element.name == command);
-  }
-}
-
-class Face {
-  constructor(schema, name, data) {
-    this.schema = schema;
-    this.name = name;
-    if (data) {
-      this.data = data;
-    } else if (this.schema.acquire) {
-      var now_script_path = config.script_path + "/" + this.schema.acquire.exec + " " + this.schema.acquire.args.join(" ");
-      //print("piping " + now_script_path);
-      var now_data = IRZ.pipe(now_script_path);
-      if (now_data)
-        this.data = JSON.parse(now_data);
-    }
-  }
-  list() {
-    if (this.schema.namesake) {
-      var facelist = [];
-      Object.getOwnPropertyNames(this.data).forEach((element) => facelist.push(this.data[element][this.schema.namesake]));
-      return facelist;
-    }
-    else
-      return Object.getOwnPropertyNames(this.data);
-  }
-  resolve(dataname) {
-    if (this.data[dataname]) {
-      //print("data: " + JSON.stringify(this.data[command]));
-      var schema_section;
-      Object.getOwnPropertyNames(this.schema.patternProperties).forEach((pattern) => {
-        let re = new RegExp(pattern);
-        if (re.test(dataname))
-          schema_section = this.schema.patternProperties[pattern];
-      });
-      //print("schema: " + JSON.stringify(schema_section));
-      return schema_section;
-    }
-    return undefined
-  }
-  traverse(command) {
-    if (this.schema.namesake) {
-      var dataname = Object.getOwnPropertyNames(this.data).find((element) => command == this.data[element][this.schema.namesake])
-      if (this.resolve(dataname))
-        return new Option(this.resolve(dataname), command, this.data[dataname])
-    }
-    else
-      if (this.resolve(command))
-        return new Option(this.resolve(command), command, this.data[command])
-    return undefined;
-  }
-}
-
-class Option {
-  constructor(schema, name, data) {
-    this.schema = schema;
-    this.name = name;
-    if (data) {
-      this.data = data;
-    } else if (this.schema.acquire) {
-      var now_script_path = config.script_path + "/" + this.schema.acquire.exec + " " + this.schema.acquire.args.join(" ");
-      //print("piping " + now_script_path);
-      var now_data = IRZ.pipe(now_script_path);
-      if (now_data)
-        this.data = JSON.parse(now_data);
-    }
-  }
-  list() {
-    return Object.getOwnPropertyNames(this.schema.properties);
-  }
-  traverse(command) {
-    //print("schema: " + JSON.stringify(this.schema.properties[command]));
-    //print("data: " + this.data[command]);
-    print(this.data[command]);
-    return undefined;
-  }
-}
 
 // mandatory function for CLI
 function interpret(cmdline)
@@ -234,7 +104,7 @@ function complete(userinput)
 print("starting CLI");
 print(IRZ.getenv("USER"));
 
-state.root = new Proto("");
+state.root = new CLI.Proto("");
 state.root.load(config.schema_path, JSON.parse(IRZ.pipe(config.script_list_path)).list);
 //state.root = new Proto("cli", JSON.parse(IRZ.pipe(config.script_list_path)).list, config.schema_path);
 state.push(state.root);
