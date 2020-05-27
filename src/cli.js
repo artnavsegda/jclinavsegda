d26b4a7e7a6f06a424a822fb6b3a53031f42bc3d
artnavsegda@gmail.com <artnavsegda@gmail.com>
2020-05-27 12:30:30 +0300

    a

--- src/cli.js
+++ src/cli.js
@@ -129,6 +129,42 @@ class Face extends Traversable {
 }
 
 class Option extends Traversable {
+  condProcess(cond, values) {
+    // return passed condition index
+    let i, j, n, c, res
+    for(i=0;i<cond.length;i++){
+      res = 0
+      c = 1
+      for(j=0;j<cond[i]["if"].length;j++){
+        let keys = Object.keys(cond[i]["if"][j])
+        for(n=0;n<keys.length;n++){
+          let k = keys[n]
+          let v = String(cond[i]["if"][j][k])
+          let x
+          if(["!"].indexOf( v.substr(0,1)) !== -1){
+            x = v.substr(0, 1)
+            v = v.substr(1, v.length)
+          }
+
+          let o = values[k]?values[k]:''
+          switch(x){
+            case "!":
+              c &= Boolean(String(o) !== String(v))
+              break
+            default:
+              c &= Boolean(String(o) === String(v))
+              break
+          }
+          // console.log('cond:', this.property_name, '|', k, x, v, '][', c)
+        }
+      }
+      res |= c
+      // console.log('cond:', this.property_name, '|', Boolean(res))
+      if(Boolean(res) === true)
+        return i
+    }
+    return -1
+  }
   constructor(schema, name, actions, data) {
     super();
     this.schema = schema;
